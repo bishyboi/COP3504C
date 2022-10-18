4a5a6d59052664dad366021d1ec94ca6de37dc09
Bishoy P <bishoypramanik@gmail.com>
2022-10-17 20:07:29 -0400

    beginning the implementation of scaledImage() in  scaler.cpp; current 7/10 on ZyLabs

--- scaler.cpp
+++ scaler.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <cmath>
 #include "Image.h"
 #include "ConsoleGfx.h"
 
@@ -10,7 +11,7 @@ unsigned char* scaledImage(unsigned char *imageData, int order);
 
 int main()
 {
-    std::cout << "Welcome to the Image Scaler!" << std::endl;
+    std::cout << "Welcome to the Image Scaler!" << std::endl << std::endl;
     std::cout << "Displaying Spectrum Image:" << std::endl;
     cg->displayImage(cg->testRainbow);
 
@@ -25,7 +26,7 @@ int main()
 
 bool menu()
 {
-    std::cout<< std::endl << std::endl;
+    std::cout<< std::endl;
     std::cout<< "Scaler Menu" << std::endl;
     std::cout<< "-----------" << std::endl;
     std::cout<< "0. Exit"     << std::endl;
@@ -37,7 +38,7 @@ bool menu()
     std::cout<< "6. Show Image Properties" << std::endl;
 
     int selection;
-    std::cout<< std::endl<< "Select a menu option: ";
+    std::cout<< std::endl<< "Select a Menu Option: ";
     std::cin>> selection;
 
     switch (selection)
@@ -88,7 +89,7 @@ bool menu()
             int order;
             std::cout<<"Enter orders of magnitude for reduction: ";
             std::cin>> order;
-            order = 1/order;
+            order *=-1;
             std::cout<<std::endl;
 
             scaledImage(image.getImageData(), order);
@@ -106,6 +107,25 @@ bool menu()
 
 unsigned char* scaledImage(unsigned char *imageData, int order)
 {
-    unsigned char* scaledImageData = nullptr;
+    unsigned char *scaledImageData = nullptr;
+
+    Image *image = new Image(imageData);
+    int adjustedOrder = order;
+    int scaleFactor = std::pow(2,adjustedOrder);
+
+    //Checking to see if scaling the image will exceed the boundaries where 1<height or width <256
+    while ((scaleFactor * int(image->getHeight()) > 256) || (scaleFactor * int(image->getWidth()) > 256) || (scaleFactor * int(image->getHeight()) < 1) || (scaleFactor * int(image->getWidth()) < 1))
+    {
+        if ((scaleFactor * int(image->getHeight()) > 256) || (scaleFactor * int(image->getWidth()) > 256))
+        {
+            adjustedOrder--; //If the scale factor is too large, decrease it
+        }else{
+            adjustedOrder++; //If the scale factor is too small, increase it
+        }
+
+        scaleFactor = std::pow(2,adjustedOrder);
+    }
+
+
     return scaledImageData;
 }
