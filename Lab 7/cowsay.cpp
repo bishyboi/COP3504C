cb5c0dc8cc00340a5f46e6a64e7ee8594850e5d4
Bishoy P <bishoypramanik@gmail.com>
2022-10-24 22:12:32 -0400

    beginning to create the menu

--- Lab 7/cowsay.cpp
+++ Lab 7/cowsay.cpp
@@ -4,22 +4,49 @@
 #include "Cow.h"
 #include "Dragon.h"
 #include "IceDragon.h"
+#include "string.h"
 using namespace std;
 
 int main(int argc, char* argv[])
 {
     HeiferGenerator hg;
     vector<Cow*> cows = hg.getCows();
-    string args = *argv;
 
-    cout<<args[2];
-    // if(args[1]).compare("-l"))
-    // {
-    //     cout<< "Cows available: ";
-    //     for (Cow *cow: cows)
-    //     {
-    //         cow->getName();
-    //     }
-    // }
+    if(strcmp(argv[1], "-l") ==0)
+    {
+        cout<< "Cows available: ";
+        for (Cow *cow: cows)
+        {
+            cout<<cow->getName()<< " ";
+        }
+    }
+
+    else if(strcmp(argv[1], "-n")==0)
+    {
+        if(strcmp(argv[2], "heifer")==0)
+        {
+            cows[0]->getImage();
+        } else if( strcmp(argv[2], "kitteh")==0)
+        {
+            cows[1]->getImage();
+        } else if( strcmp(argv[2], "dragon")==0)
+        {
+            cows[2]->getImage();
+        } else if( strcmp(argv[2], "ice-dragon")==0)
+        {
+            cows[3]->getImage();
+        }else{
+            cout<< "Couldn't find " << argv[2] << " cow!";
+            break;
+        }
+
+        for (int i=3; i<argc; i++)
+        {
+            cout<< argv[i] << " ";
+        }
+    }
+    else{
+
+    }
     return 0;
 }
