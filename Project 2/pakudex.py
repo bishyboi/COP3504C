655420d79a5e71197447f5f9321f83ae8e1e77b8
Bishoy P <bishoypramanik@gmail.com>
2022-10-26 22:23:01 -0400

    pakuri should be final and pakudex requires implementation for the error exceptions

---
+++ Project 2/pakudex.py
@@ -0,0 +1,131 @@
+from pakuri import Pakuri
+
+
+class Pakudex():
+    pakuris = []
+
+    def __init__(self):
+        pass
+
+    def get_species_List(self):
+        if len(self.pakuris) == 0:
+            return None
+        else:
+            species_list = []
+            for pakuri in self.pakuris:
+                species_list.append(pakuri.get_species())
+
+            return species_list
+
+    def get_stats(self, species: str):
+        if len(self.pakuris) == 0:
+            return None
+        else:
+            stats_list = []
+            for pakuri in self.pakuris:
+                if pakuri.get_species() == species:
+                    stats_list.append(pakuri.level)
+                    stats_list.append(pakuri.cp)
+                    stats_list.append(pakuri.hp)
+                    return stats_list
+
+    def sort_pakuri(self):
+        self.pakuris.sort()
+        return self.pakuris
+
+    def add_pakuri(self, species: str, level: int):
+        self.pakuris.append(Pakuri(species, level))
+        return True
+
+    def remove_pakuri(self, species: str):
+        for i in range(len(self.pakuris)):
+            if self.pakuris[i].get_species() == species:
+                self.pakuris.pop(i)
+                return True
+
+        return False
+
+    def evolve_species(self, species: str):
+        for i in range(len(self.pakuris)):
+            if self.pakuris[i].get_species() == species:
+                self.pakuris[i].level = self.pakuris[i].level * 2
+                self.pakuris[i].set_attack(self.pakuris[i].get_attack() + 1)
+                return True
+
+        return False
+
+
+def menu(pakudex: Pakudex):
+    print()
+    print("Pakudex Main Menu")
+    print("-"*17)
+    print("1. List Pakuri")
+    print("2. Show Pakuri")
+    print("3. Add Pakuri")
+    print("4. Remove Pakuri")
+    print("5. Evolve Pakuri")
+    print("6. Sort Pakuri")
+    print("7. Exit")
+    print()
+    selection = int(input("What would you like to do? "))
+
+    if selection == 1:
+        species_list = pakudex.get_species_List()
+        if species_list != None:
+            print("Pakuri in Pakudex")
+            for i in range(len(species_list)):
+                print(f"{i+1}. {species_list[i]}")
+        else:
+            print("No Pakuri currently in Pakudex!")
+
+    elif selection == 2:
+        species = input("Enter the name of the species to display: ")
+        
+        pakuri_info = pakudex.get_stats(species)
+
+        if pakuri_info==None:
+            print("Error: No such Pakuri!")
+        else:
+            print()
+            print(f"Species: {species}")
+            print(f"Level: {pakuri_info[0]}")
+            print(f"CP: {pakuri_info[1]}")
+            print(f"HP: {pakuri_info[2]}")
+
+    elif selection == 3:
+        species = input("Species: ")
+        level = int(input("Level: "))
+        pakudex.add_pakuri(species, level)
+        print(f"Pakuri species {species} (level {level}) added!")
+
+    elif selection == 4:
+        species = input("Enter the name of the Pakuri to remove: ")
+        pakudex.remove_pakuri(species)
+        print(f"Pakuri {species} removed.")
+
+    elif selection == 5:
+        species = input("Enter the name of the species to evolve: ")
+        pakudex.evolve_species(species)
+        print(f"{species} has evolved!")
+
+    elif selection == 6:
+        pakudex.sort_pakuri()
+        print("Pakuri have been sorted!")
+    elif selection == 7:
+        print("Thanks for using Pakudex! Bye!")
+        return False
+
+    return True
+
+
+def main():
+    pakudex = Pakudex()
+    print("Welcome to Pakudex: Tracker Extraordinaire!")
+
+    run = True
+    while (run):
+        run = menu(pakudex)
+
+
+if __name__ == "__main__":
+    main()
--- Project 2/pakuri.py
+++ Project 2/pakuri.py
@@ -1,3 +1,5 @@
+import math
+
 class Pakuri:
     __species:str
     __level: int
@@ -12,5 +14,44 @@ class Pakuri:
          self.__attack = (len(species) * 7 + 11) % 16
          self.__defense = (len(species) * 5  + 17) % 16
          self.__stamina = (len(species) * 6 + 13) % 16
-         self.hp = (self.__stamina * self.__level)//6
-         self.cp = (self.__attack * self.__level * self.__defense**0.5 * self.__stamina**0.5)
+         
+    def get_species(self):
+        return self.__species
+    
+    def get_attack(self):
+        return self.__attack
+    
+    def get_defense(self):
+        return self.__defense
+    
+    def get_stamina(self):
+        return self.__stamina
+    
+    def set_attack(self, new_attack: int):
+        self.__attack = new_attack
+        
+    def __lt__(self, other):
+        if self.__species < other.get_species():
+            return True
+        return False
+    
+    def __gt__(self, other):
+        if self.get_species() > other.get_species():
+            return True
+        return False
+        
+    @property
+    def cp(self):
+        return math.floor((self.__attack * self.__level * self.__defense**0.5 * self.__stamina**0.5)/12.5)
+    
+    @property
+    def hp(self):
+        return (self.__stamina * self.__level)//6
+    
+    @property
+    def level(self):
+        return self.__level
+    
+    @level.setter
+    def level(self, new_level:int):
+        self.level = new_level
